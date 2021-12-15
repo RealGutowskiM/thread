15f5f2a8476dcf040f7f9a7a0e9878f7ecc3c127
Michał Gutowski <michal.mg.gutowski@gmail.com>
2021-12-15 11:21:07 +0100

    init

--- client/index.ts
+++ client/index.ts
@@ -1,1 +1,5 @@
-console.log( "hello world!" )
+console.log( "hello world!" );
+
+if ( process.env.NODE_ENV !== "production" ) {
+    console.log( "im prod ready!" );
+}
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,28 @@
 import { createServer } from "node:http";
+import { resolve } from "node:path";
+import { spawn } from "node:child_process";
 
-createServer().listen( "8080" ).on( "listening", () => {
-    console.log( "listening on port 8080" );
+const port = process.env.PORT || 8080;
+
+createServer().listen( port ).on( "listening", () => {
+    console.log( "listening on", port );
 } );
+
+if ( process.env.NODE_ENV !== "production" ) {
+    let client = spawn(
+        "npm", [ "run", "watch" ],
+        { cwd: resolve( __dirname, ".." ) }
+    );
+
+    client.stdout.on( 'data', ( data ) => {
+        console.log( `parcel: ${data}` );
+    } );
+
+    client.stderr.on( 'data', ( data ) => {
+        console.error( `parcel error: ${data}` );
+    } );
+
+    client.on( 'close', ( code ) => {
+        console.log( `parcel exited with code ${code}` );
+    } );
+}
